575b992d836366c8e91f8e4094956c774c4890ba
Manii1997 <manibct1817@gmail.com>
2026-06-18 15:47:01 +0530

    Refactor Contact component layout and styling for improved readability and consistency

--- src/components/ContactSection/Contact.jsx
+++ src/components/ContactSection/Contact.jsx
@@ -6,41 +6,41 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="contact-section py-5 md:ml-72 flex flex-col gap-10 bg-[#0F172A] text-white"
+      className="flex flex-col gap-10 bg-[#0F172A] md:ml-72 py-5 text-white contact-section"
     >
-      <div className="p-3 flex flex-col grow justify-center">
-        <h1 className="text-4xl md:text-6xl font-bold self-center">Contact</h1>
+      <div className="flex flex-col justify-center p-3 grow">
+        <h1 className="self-center font-bold text-4xl md:text-6xl">Contact</h1>
         <p className="mt-10 text-center">
           If you would like to discuss further, please contact me.
         </p>
-        <div className="flex flex-col md:flex-row">
+        <div className="flex md:flex-row flex-col">
           <div className="md:w-1/2">
-            <div className="flex flex-col self-center p-5 py-10 border mt-5 rounded md:m-20">
+            <div className="flex flex-col self-center md:m-20 mt-5 p-5 py-10 border rounded">
               <img
                 src="https://res.cloudinary.com/drdl4pdnx/image/upload/v1718092069/Portfolio-Website/accept-call-icon_kfjb56.svg"
                 alt="call"
-                className="h-[50px] md:h-[80px] self-center"
+                className="self-center h-[50px] md:h-[80px]"
               />
-              <p className="mt-5 text-center text-xl">
-                <span className="font-semibold ">Phone</span> <br />
-                <a href="tel:+918675748207" className="hover:underline">
-                  +91 86757 48207
+              <p className="mt-5 text-xl text-center">
+                <span className="font-semibold">Phone</span> <br />
+                <a href="tel:+918148526264" className="hover:underline">
+                  +91 81485 26264
                 </a>
               </p>
             </div>
           </div>
           <div className="md:w-1/2">
-            <div className="flex flex-col self-center px-5 py-10 border mt-5 rounded md:m-20">
+            <div className="flex flex-col self-center md:m-20 mt-5 px-5 py-10 border rounded">
               <img
                 src="https://res.cloudinary.com/drdl4pdnx/image/upload/v1718092069/Portfolio-Website/gmail-icon_odbyt0.svg"
                 alt="email"
-                className="h-[50px] md:h-[80px] self-center"
+                className="self-center h-[50px] md:h-[80px]"
               />
-              <p className="mt-5 text-center text-xl">
+              <p className="mt-5 text-xl text-center">
                 <span className="font-semibold">Email</span> <br />
                 <a
                   href="mailto:manideveloper.designer@gmail.com"
-                  className=" hover:underline"
+                  className="hover:underline"
                 >
                   manideveloper.designer@gmail.com
                 </a>
